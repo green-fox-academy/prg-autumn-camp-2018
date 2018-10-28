7e5f05922eaea4c479894bc0b208305669b5a768
Adam Gyulavari <adam.gyulavari@lab.coop>
2018-10-28 14:56:08 +0100

    add 4th day examples

--- examples/scripts.js
+++ examples/scripts.js
@@ -10,3 +10,49 @@ colors.forEach(oneColor => {
     console.log('Just another color:' + oneColor);
   }
 });
+
+$('ul').append('<li class="aze">hello</li>');
+
+let names = ['Aze', 'Alex', 'Lucie'];
+names.forEach(name => {
+  $('ul').append(`<li class="Lucie">${name}</li>`)
+  if(name === 'Lucie') {
+    
+  }
+})
+// This part is done. Below
+$('.unicorn').css('font-weitht', 'bold')
+
+// Start saving something
+
+let path = `pony/something/else/1`;
+let dataToSave = {
+  title: "My second saved blog post",
+  text: "Some hilarious content, which proves how awesome I am.",
+  image: "https://img.cncenter.cz/img/11/normal510/2826710-img-baracnicka-rychta-v0.jpg?v=0"
+};
+
+fb.ref(path).set(dataToSave);
+
+let initialData = [
+  {
+    title: "First Post",
+    text: "Text of the first one"
+  },
+  { 
+    title: "Second",
+    text: "second post's text"
+  }
+];
+
+fb.ref("posts").once('value').then(data => {
+  let savedPosts = data.val();
+  savedPosts.forEach(post => {
+    $('body').append(`
+      <h1>${post.title}</h1>
+      <p>${post.text}</p>
+    `);
+  });
+});
+
+
